65e71a7d5286324ec6d935578e0f4b104a0d9ad5
natsukui <natsukui@gmail.com>
2020-06-15 16:12:43 +0900

    一通り2

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,5 +1,8 @@
 import click
 
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
+
 @click.command()
 @click.option('--message', '-m', default='LGTM', show_default=True, help='画像に載せる文字列')
 @click.argument('keyword')
@@ -11,4 +14,6 @@ def cli(keyword, message):
 	click.echo('LGTM')
 
 def lgtm():
-	pass
+	with get_image(keyword) as fp:
+		save_with_message(fp, message)
+		
